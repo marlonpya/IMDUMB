4fc7ddc187a9dbd0e58fd0f7670c3cdcb0b310e8
arteaga <marlon.arteaga.m@hotmail.com>
2026-02-04 10:59:42 -0500

    Initial Commit

---
+++ IMDUMB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDUMB
+//
+//  Created by Julio Alexis Arteaga Morales on 3/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
